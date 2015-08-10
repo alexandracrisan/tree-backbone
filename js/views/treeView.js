92488977c1678da181986ded443c2613b36bed68
Alexandra <=>
2015-08-10 14:56:47 +0300

    icons added final

--- js/views/treeView.js
+++ js/views/treeView.js
@@ -13,14 +13,13 @@ window.SearchTree.treeView = Backbone.View.extend({
         list: _.template(
             '<ul>' +
             '<% _.each(data, function (item) { %>' +
-             '<% if(item.hasOwnProperty("children")){ %>' +
-             //'<% .li.class="mdi mdi-folder" %>' +
+            //'<li><% (item.hasOwnProperty("children")) ? class="mdi mdi-folder"<%= item.name %> : class="mdi mdi-file-outline"<%= item.name %> %></li>'+
+            '<% if(item.hasOwnProperty("children")){ %>' +
             '<li class="mdi mdi-folder"><%= item.name %></li>' +
-            //'<% this.templates.list({data: item.children}) %>' +
             '<% } %>' +
-            //'<% else { %>' +
+            '<% if(!item.hasOwnProperty("children")){ %>' +
             '<li class="mdi mdi-file-outline"><%= item.name %></li>' +
-            //'<% } %>' +
+            '<% } %>' +
             '<% }) %>' +
             '</ul>'
         )
@@ -41,23 +40,14 @@ window.SearchTree.treeView = Backbone.View.extend({
     }
 });
 
-//<td><%= compensation %></td>
-//
-//SearchTree.CreateTree = function(data, parent){
-//    var container = document.createElement('ul');
-//    for(var i=0; i < data.length; i++) {
-//        var entry = data[i];
-//        var li = document.createElement('li');
-//        li.innerHTML = entry.name;
-//        li.className = SearchTree.constants.classes.fileIcon;
-//        container.appendChild(li);
-//        if (entry.hasOwnProperty('children')) {
-//            SearchTree.CreateTree(entry.children, li);
-//            li.className = SearchTree.constants.classes.folderIcon;
-//        }
-//    }
-//    parent.appendChild(container);
-//}
-//<% if (typeof(date) !== "undefined") { %>
-//<span class="date"><%= date %></span>
-//        <% } %>
+////<tbody>
+//<% _.each(accountLists, function(account) { %>
+//    <tr>
+//    <td><%= (account.active == 'true') ? 'Active': 'Inactive' %></td>
+//    </tr>
+//    <% }); %>
+//</tbody>
+
+
+//'<% this.templates.list({data: item.children}) %>' +
+//'<% .li.class="mdi mdi-folder" %>' +
